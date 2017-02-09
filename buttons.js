a50050177e3327d70442609deb82a1b6d088660b
Jacob Feliciani <Jake13f@hotmail.com>
2017-02-09 18:05:15 -0500

    Added multiple styles, differnet trials, and the results page

--- buttons.js
+++ buttons.js
@@ -5,10 +5,34 @@ $(function () {
    var counter = $("#counter");
    var reset = $("#reset");
    var time = $("#timer");
+   var startbtn = $("#start");
 
    var timer;
    var totalTime = 0;
    var count = 0;
+   var trials = {
+      times1: [],
+      times2: [],
+      times3: [],
+      current: 1
+   };
+
+   // Handler for start button
+   startbtn.on('click', function () {
+      // Hide the start window
+      $(this).parent().hide();
+
+      // Start timer
+      timer = setInterval(
+         function () {
+            totalTime++;
+            time.text(inSeconds(totalTime));
+         }, 10
+      );
+
+      // Turn on a button to start with
+      turnOn(lbtn);
+   });
 
    // Event handler for the button clicks
    btns.on("click", function () {
@@ -28,20 +52,78 @@ $(function () {
       count++;
       counter.text(count); // Print the number of clicks to the screen
 
-      if (count == 1) {
-         timer = setInterval(
-            function () {
-               totalTime++;
-               time.text(inSeconds(totalTime));
-            }, 10
-         );
-      } else if (count == 10) {
+      if (count < 10) {
+         trials['times'+trials.current].push(totalTime);
+      } else {
          clearInterval(timer);
+         trials['times'+trials.current].push(totalTime);
          count = totalTime = 0; // Reset
 
          // Disable both buttons since completed
          turnOff(lbtn);
          turnOff(rbtn);
+
+         trials.current++;
+         if (trials.current == 2) {
+            $('.text').html("<h2>Trial 2</h2>");
+            shrink(lbtn, rbtn);
+            lbtn.css('top', '0');
+            lbtn.css('left', '0');
+            rbtn.css('top', '100%');
+            rbtn.css('right', '0');
+         } else if (trials.current == 3) {
+            $('.text').html("<h2>Trial 3</h2>");
+            shrink(lbtn, rbtn);
+            lbtn.css('top', '100%');
+            lbtn.css('left', '0');
+            rbtn.css('top', '0');
+            rbtn.css('right', '0');
+         } else {
+            var trialHTML = `
+               <h2 class='center-txt'>RESULTS<h2>
+               <table>
+                  <thead>
+                     <tr>
+                        <th>Click #</th>
+                        <th>Stage 1</th>
+                        <th>Stage 2</th>
+                        <th>Stage 3</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+            `;
+            var total = [0,0,0];
+            trials.times1.forEach(function (trial, i) {
+               total[0] += trials.times1[i];
+               total[1] += trials.times2[i];
+               total[2] += trials.times3[i];
+
+               trialHTML += `
+                  <tr>
+                     <td>`+(i+1)+`</td>
+                     <td>`+inSeconds(trials.times1[i])+`</td>
+                     <td>`+inSeconds(trials.times2[i])+`</td>
+                     <td>`+inSeconds(trials.times3[i])+`</td>
+                  </tr>
+               `;
+            });
+            trialHTML += `
+               <tr>
+                  <td><b>TOTAL</b></td>
+                  <td><b>`+inSeconds(total[0])+`</b></td>
+                  <td><b>`+inSeconds(total[1])+`</b></td>
+                  <td><b>`+inSeconds(total[2])+`</b></td>
+               </tr>
+            `;
+
+            trialHTML += `
+                  </tbody>
+               </table>
+            `;
+
+            $('body').html(trialHTML);
+         }
+         startbtn.parent().show();
       }
    });
 
@@ -49,11 +131,16 @@ $(function () {
    reset.on("click", function () {
       clearInterval(timer);
       count = totalTime = 0;
+      trails.current = 1;
       time.text(inSeconds(totalTime));
       counter.text(count);
 
       turnOn(lbtn);
       turnOn(rbtn);
+
+      // TODO: Remove when completed - testing
+      // lbtn.click();
+      // rbtn.click();
    });
 
    // Toggle the disabled attribute on or off
@@ -61,4 +148,14 @@ $(function () {
    function turnOn (element) { element.prop('disabled', false); }
    function turnOff (element) { element.prop('disabled', true); }
    function inSeconds (t) { t = t / 100; return t.toFixed(2); }
+
+   // Shrink the button size
+   // @param b1 - the first button element
+   // @param b2 - the second button element
+   function shrink (b1, b2) {
+      b1.width(b1.width() / 2);
+      b1.height(b1.height() / 2);
+      b2.width(b2.width() / 2);
+      b2.height(b2.height() / 2);
+   }
 });
